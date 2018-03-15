261b44ab7abe4a84263d654bc9c1a30053c1edc6
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-15 17:00:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word (phrase)
   phraseArray=phrase.split (" ")
   msg="";
   phraseArray.each do |word|
-    if(msg.length==0)
+    if(msg == "")
       msg=msg + word.reverse
     else
       msg=msg + " "+ word.reverse
